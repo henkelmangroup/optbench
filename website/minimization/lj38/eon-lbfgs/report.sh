c3b7d5c9393eb81ae45896da379c8b5f35163854
Sam Chill <samchill@gmail.com>
2012-11-07 18:24:01 +0000

    Update eon lbfgs description and code version.

--- website/minimization/lj38/eon-lbfgs/report.sh
+++ website/minimization/lj38/eon-lbfgs/report.sh
@@ -14,9 +14,9 @@ echo "force_calls_max $fcalls_max" >> benchmark.dat
 echo "force_calls_min $fcalls_min" >> benchmark.dat
 fc_per_second=$(awk "BEGIN {print $fcalls / $walltime}")
 echo "force_calls_per_second $fc_per_second" >> benchmark.dat
-echo "algorithm L-BFGS" >> benchmark.dat
+echo "algorithm L-BFGS M=5 maxstep=0.2" >> benchmark.dat
 echo "nfailed $nfailed" >> benchmark.dat
 echo "code Eon" >> benchmark.dat
-echo "code_version r1789" >> benchmark.dat
+echo "code_version r1963" >> benchmark.dat
 echo "date $(date +'%d %b %Y')" >> benchmark.dat
 echo "contributor Sam Chill" >> benchmark.dat
