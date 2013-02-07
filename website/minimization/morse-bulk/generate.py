5b0325ebdc53ee03f3dc5a67f194b0c335ed23ac
Sam Chill <samchill@gmail.com>
2013-02-07 22:13:25 +0000

    added morse bulk benchmark

---
+++ website/minimization/morse-bulk/generate.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from ase.lattice.cubic import FaceCenteredCubic
+from tsase.io import write_con
+a=4.0969766901948566
+atoms = FaceCenteredCubic(size=(4,4,4), symbol='Pt', pbc=(1,1,1),latticeconstant=a)
+
+for i in xrange(100):
+    atoms_disordered = atoms.copy()
+    atoms.rattle(.05, seed=100*i)
+    write_con('pos_%.4i.con'%i, atoms)
