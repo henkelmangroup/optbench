66acf39a8e5c0ed73dc26c26a435579c7d6948fc
Jacob Stevenson <js850@cam.ac.uk>
2013-06-24 21:50:31 +0000

    remove the pele python only implemetation

--- website/minimization/lj38/pygmin-lbfgs-M4/report.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash 
-python ../gmin_getdata.py "/home/js850/research/benchmark/henkelman/benchmarks/bin/js850/minimization/pygmin/lbfgs_py-M4-maxstep0.1.ncalls" "L-BFGS M=4 maxstep=0.1 (python)" PyGMIN
