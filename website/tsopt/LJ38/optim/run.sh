22dad351da90487ec59869346fa497332ce1fc4d
Victor Ruehle <victor.ruehle@gmail.com>
2013-06-25 21:28:26 +0000

    optim for ts search

---
+++ website/tsopt/LJ38/optim/run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -e
+
+if [ -e simulation ]; then
+    rm -rf simulation
+fi
+mkdir simulation && cd simulation
+tar xzf ../../coords-xyz.tar.gz
+
+for i in {0..199}
+do
+    file=$(printf "%.4i.xyz" $i)
+    mkdir run-$i && cd run-$i
+    echo run-$i
+    cp ../../odata.preamble .
+    ../../run_optim.sh ../coords-xyz/$file 
+    cd ..
+done
+
+python ../getdata.py
+mv benchmark.dat ..
+cd ..
