b34d851b2e342f8817832f2cc3724f1bc371bcda
Sam Chill <samchill@gmail.com>
2013-06-25 21:15:12 +0000

    up

--- website/tsopt/LJ38/eon-dimer/run.sh
+++ website/tsopt/LJ38/eon-dimer/run.sh
@@ -11,13 +11,15 @@ for i in {0..199}
 do
     file=$(printf "%.4i.con" $i)
     mkdir -p run-$i
-    echo run-$i
+    /bin/echo -n run-$i 
     cp coords-con/$file run-$i/pos.con
     cp coords-con/$file run-$i/displacement.con
     cp ../config.ini run-$i
+    cp ../in.lammps run-$i
     cd run-$i
     ../../random_mode.py $i
     eonclient > stdout.dat
+    grep total_force_calls results.dat | awk '{print " ", $1}'
     cd ..
 done
 cd ..
