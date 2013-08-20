4384769c2fbaf5184c4bc7f0375d62cf0ceb47f9
Sam Chill <samchill@gmail.com>
2013-08-20 09:34:54 +0000

    start of eon-basinhopping

---
+++ website/globalopt/lj38/eon-basinhopping/run.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+set -e
+
+if [ -e runs ]; then
+    rm -rf runs
+fi
+mkdir -p runs
+cd runs
+tar xfz ../../100_lj38.tar.gz
+for i in {0..99}
+do
+    file=$(printf "%i.con" $i)
+    mkdir -p run-$i
+    /bin/echo -n run-$i
+    cp 100_lj38/$file run-$i/pos.con
+    cp ../config.ini run-$i
+    cp ../in.lammps run-$i
+    cd run-$i
+    eonclient > stdout.dat
+    grep total_force_calls results.dat | awk '{print " ", $1}'
+    cd ..
+done
+cd ..
+./report.sh
