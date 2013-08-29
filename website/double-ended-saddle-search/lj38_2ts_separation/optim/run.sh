7e4ce3e81dfbf5ac0da3c2a84a2480d01d9481cd
Jacob Stevenson <js850@cam.ac.uk>
2013-08-29 16:39:02 +0000

    add optim long distance double ended connect

---
+++ website/double-ended-saddle-search/lj38_2ts_separation/optim/run.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -e
+
+if [ -e simulation ]; then
+    rm -rf simulation
+fi
+mkdir simulation && cd simulation
+tar xzf ../coords.tgz
+
+for i in {0..49}
+do
+    f1=$(printf "start_%d.xyz" $i)
+    f2=$(printf "end_%d.xyz" $i)
+    mkdir run-$i && cd run-$i
+    echo run-$i $f1 $f2
+    cp ../../odata.preamble .
+    cp odata.preamble odata
+    
+    awk '(NR>2){print $2, $3, $4}' ../coords/$f2  | sed 's/[eE]/D/g' > finish
+    ../../run_optim.sh ../coords/$f1 
+    cd ..
+done
+
+python ../getdata.py
+
+echo "algorithm DNEB + hybrid eigenvector following + Djikstra shortest path" >> benchmark.dat
+echo "code OPTIM" >> benchmark.dat
+echo "contributor Jacob Stevenson" >> benchmark.dat
+
+
+mv benchmark.dat ..
+cd ..
+
