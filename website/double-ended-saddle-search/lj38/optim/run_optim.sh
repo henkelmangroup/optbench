75c380a610ba42e1581fa3095a07b9d0d8533763
Jacob Stevenson <js850@cam.ac.uk>
2013-08-30 13:57:15 +0000

    fix optim benchmark for double ended saddle search

--- website/double-ended-saddle-search/lj38/optim/run_optim.sh
+++ website/double-ended-saddle-search/lj38/optim/run_optim.sh
@@ -10,12 +10,10 @@ ncalls=$(grep "energy+gradient calls" OPTIM.log  | tail -1 | awk '{print $6}' ||
 #rms=$(grep "energy+gradient calls" OPTIM.log  | tail -1 | awk '{print $6}')
 #energy=$(grep "mylbfgs> Final energy is" OPTIM.log  | tail -1 | awk '{print $5}')
 #eigenval=$(grep "xmylbfgs> Eigenvalue and RMS=" OPTIM.log  | tail -1 | awk '{print $5}')
-success=$(grep "Connected path found" OPTIM.log > /dev/null && echo 1 || echo 0)
-nts=$(grep "Number of TS in the path" OPTIM.log  | tail -1 | awk '{print $8}' || echo $tmp)
-ncycles=$(grep "Number of cycles" OPTIM.log  | tail -1 | awk '{print $5}' || echo $tmp)
+success=$(grep "Converged to TS" OPTIM.log > /dev/null && echo 1 || echo 0)
 
 if [ "$ncalls" == "" ]; then ncalls="-1"; fi
-if [ "$nts" == "" ]; then rms="-1"; fi
-if [ "$ncycles" == "" ]; then energy="-1"; fi
+#if [ "$nts" == "" ]; then nts="-1"; fi
+if [ "$ncycles" == "" ]; then ncycles="-1"; fi
 
-echo "$(basename $1) $ncalls $nts $ncycles $success" >> ../results.txt
+echo "$(basename $1) $ncalls $success" >> ../results.txt
