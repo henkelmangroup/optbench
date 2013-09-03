059808262302e216ec817b8d85a9a15cd5d46e61
Jacob Stevenson <js850@cam.ac.uk>
2013-09-03 10:21:47 +0000

    add a script to convert con to coords files

---
+++ bin/js850/con2coords.py
@@ -0,0 +1,28 @@
+import argparse
+import numpy as np
+
+from tools import read_con_file
+
+def main():
+    parser = argparse.ArgumentParser(description="convert a con file to a coords file")
+    parser.add_argument("confile", type=str, help="file to read from")
+    parser.add_argument("outfile", type=str, help="file to write to")
+    parser.add_argument("--frozen", type=str, help="file to write to write frozen atoms to", default="")
+    args = parser.parse_args()
+    
+    res = read_con_file(args.confile)
+    
+    coords = res.coords.reshape([-1,3])
+    np.savetxt(args.outfile, coords, fmt="%.16f")
+    
+    if args.frozen != "":
+        frozen_indices = np.where(res.frozen)
+        print frozen_indices
+        
+    
+
+    
+    
+
+if __name__ == "__main__":
+    main()
