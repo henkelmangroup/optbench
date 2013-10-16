4d8f3a938dad7ed7767e918e56f02f68905bc1ea
Catherine Smith <shmidly222@hotmail.com>
2013-10-16 23:12:43 +0000

    update results in /double-ended/lj38 by optim

--- website/double-ended-saddle-search/lj38/optim/run.sh
+++ website/double-ended-saddle-search/lj38/optim/run.sh
@@ -24,7 +24,7 @@ python ../getdata.py
 
 echo "algorithm DNEB + hybrid eigenvector following" >> benchmark.dat
 echo "code OPTIM" >> benchmark.dat
-echo "contributor Jacob Stevenson" >> benchmark.dat
+echo "contributor Jacob Stevenson & Cheng Shang" >> benchmark.dat
 
 
 mv benchmark.dat ..
