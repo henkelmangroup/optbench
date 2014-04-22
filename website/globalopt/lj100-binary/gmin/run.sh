2499dd3f2f7f1bab073f338dd18ab6915009e75c
Jacob Stevenson <js850@cam.ac.uk>
2014-04-22 11:14:41 +0000

    add GMIN benchmark for BLJ100
    run.sh is not fully working yet.  benchmark.dat was added by hand from a
    previous benchmark

---
+++ website/globalopt/lj100-binary/gmin/run.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -e
+
+if [ -e simulation ]; then
+    rm -rf simulation
+fi
+mkdir simulation && cd simulation
+tar xzf ../lj100-binary-con.tgz
+
+for i in {0..99}
+do
+    file=$(printf "../lj100-binary-con/coords.%d" $i)
+    mkdir run-$i && cd run-$i
+    echo run-$i
+    cp ../../data .
+    cp $file coords
+    ../../run_gmin.sh $file
+    cd ..
+done
+
+python ../getdata.py --datafile=results.txt
+
+cat ../admin.dat >> benchmark.dat
+
+
+mv benchmark.dat ..
+cd ..
+
