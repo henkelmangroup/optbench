2f0c1e685cf21a604365ebcf066b5ef93fd6a969
Penghao Xiao <xphdream@gmail.com>
2014-06-19 02:46:16 +0000

    add scripts for a ready run

--- website/lowest-eigenvector/lj38/tsase-lanczos/run.sh
+++ website/lowest-eigenvector/lj38/tsase-lanczos/run.sh
@@ -6,15 +6,15 @@ if [ -e runs ]; then
 fi
 mkdir -p runs
 cd runs
-#tar xfz ../coords-con.tar.gz
+tar xfz ../coords-con.tar.gz
 for i in {0..199}
 do
     file=$(printf "%.4i.con" $i)
     mode_file=$(printf "%.4i_mode" $i)
     mkdir -p run-$i
     /bin/echo -n run-$i
-    cp ../coords-con/$file run-$i/pos.con
-    cp ../coords-con/$mode_file run-$i/true_mode
+    cp coords-con/$file run-$i/pos.con
+    cp coords-con/$mode_file run-$i/true_mode
     cp ../*.py run-$i
     cd run-$i
     ../../random_mode.py $i
