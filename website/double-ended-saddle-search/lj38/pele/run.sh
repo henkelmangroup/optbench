3366fcec93a3436b5f57983ca74021b4b3df3bad
Jacob Stevenson <js850@cam.ac.uk>
2014-08-06 15:12:59 +0000

    update pele lj38 double ended saddle search

--- website/double-ended-saddle-search/lj38/pele/run.sh
+++ website/double-ended-saddle-search/lj38/pele/run.sh
@@ -5,3 +5,5 @@ python dec_lj38.py
 python getdata.py
 
 rm -r coords/
+
+echo "code_file pele-$(python get_pele_version.py).tgz" >> benchmark.dat
--- website/double-ended-saddle-search/pt-island/pele/run.sh
+++ website/double-ended-saddle-search/pt-island/pele/run.sh
@@ -3,3 +3,5 @@ tar -xzf pt-island-con.tgz
 python dec_morse.py
 python getdata.py
 rm -r pt-island-con
+
+echo "code_file pele-$(python get_pele_version.py).tgz" >> benchmark.dat
