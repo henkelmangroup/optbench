d0a37468c38ca673ef665a7cae7c89e90a57dc44
Jacob Stevenson <js850@cam.ac.uk>
2014-08-06 15:12:38 +0000

    update pele-fire

--- website/minimization/lj38/pele-fire/run.sh
+++ website/minimization/lj38/pele-fire/run.sh
@@ -7,3 +7,5 @@ python minimization_fire.py --maxstep=$maxstep
 python minimization_getdata.py "results_data.ncalls" "Fire" pele
 
 rm -r lj38-clusters
+
+echo "code_file pele-$(python get_pele_version.py).tgz" >> benchmark.dat
