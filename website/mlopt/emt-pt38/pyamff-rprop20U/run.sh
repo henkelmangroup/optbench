3fdb0becaac27b553e6d559dd294add24ff75987
Akksay Singh <akksay@gmail.com>
2022-12-12 13:47:17 +0000

    does this remove entry?

--- website/mlopt/emt-pt38/pyamff-rprop20U/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#python mlrf0_optb.py 50
-#python mlrf0_optb.py -r
-python mlrf0_optb.py -s
-echo "code Pyamff" >> benchmark.dat
-echo "code_version 8037eb98" >> benchmark.dat
-echo "date $(date +'%d %b %Y')" >> benchmark.dat
-echo "contributor Henkelman Group" >> benchmark.dat
-echo "code_file pyamff-8037eb98.tgz" >> benchmark.dat
